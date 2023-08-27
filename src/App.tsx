70bb0f013ae6745d6bf8deece7fc2148e4ea2fc1
Mark P <mj.poso18@gmail.com>
2023-08-28 02:49:45 +0800

    Create temporary landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,10 @@
-import { Link, Outlet, Route } from "react-router-dom";
-
-function NavBar() {
-  return (
-    <nav className="flex justify-between">
-      <div>
-        <Link to="/">Solar Crown</Link>
-      </div>
-      {/* <div className="place-self-center"> */}
-      {/* <Link to="/catalog">Catalog</Link> */}
-      {/* </div> */}
-      <div className="place-self-end flex gap-3">
-        <button>Dark</button>
-        <Link to="/cart">Cart</Link>
-      </div>
-    </nav>
-  );
-}
-
-function LandingView() {
-  return (
-    <>
-      <p>Hello, world!</p>
-      <Link
-        className="uppercase tracking-widest text-sm bg-slate-800 text-white px-3 py-2"
-        to="/catalog"
-      >
-        Browse
-      </Link>
-    </>
-  );
-}
-
-function CatalogView() {
-  return <p>Catalog</p>;
-}
+import { Outlet, Route } from "react-router-dom";
+import { LandingView } from "./view/LandingView.tsx";
 
 function App() {
   return (
-    <div className="grid gap-3 p-4">
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      {/* <footer></footer> */}
+    <div className="select-none">
+      <Outlet />
     </div>
   );
 }
@@ -52,6 +12,5 @@ function App() {
 export const AppTree = (
   <Route path="/" element={<App />}>
     <Route index element={<LandingView />} />
-    <Route path="/catalog" element={<CatalogView />} />
   </Route>
 );
---
+++ src/view/LandingView.tsx
@@ -0,0 +1,14 @@
+// TODO Image logo?
+// TODO Pulsating logo?
+export function LandingView() {
+  return (
+    <main className="h-screen grid bg-neutral-900 text-white ">
+      <div className="relative place-self-center px-4 py-3 font-thin text-5xl tracking-wider bg-neutral-50/10">
+        Solar Crown
+        <button className="absolute top-full left-1/2 -translate-x-1/2 w-max mt-2 px-5 py-3 rounded-full text-sm tracking-widest uppercase bg-black">
+          View Catalog
+        </button>
+      </div>
+    </main>
+  );
+}
