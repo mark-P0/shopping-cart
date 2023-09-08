ad3574e4826408c1b9d786e5987b61191e2c402c
Mark P <mj.poso18@gmail.com>
2023-09-09 06:29:12 +0800

    Configure Vite to automatically use repo stub

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  /**
+   * Use relative base path so that assets will resolve to the
+   * repo path (e.g. <user>.github.io/<repo>) instead of the
+   * root URL (<user>.github.io)
+   *
+   * - https://vitejs.dev/config/shared-options.html#base
+   * - https://vitejs.dev/guide/static-deploy.html#github-pages
+   */
+  base: "./",
 });
