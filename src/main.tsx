164dbd7e0e7cfd496f53ad9dfe75a6aa73be87bb
Mark P <mj.poso18@gmail.com>
2023-09-09 06:11:22 +0800

    Use hash router for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   RouterProvider,
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import { AppTree } from "./App.tsx";
 import "./index.css";
 
-const router = createBrowserRouter(createRoutesFromElements(AppTree));
+const router = createHashRouter(createRoutesFromElements(AppTree));
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
