35d5423174617c7596c6f0ab5416d5eebfa7bd92
Mark P <mj.poso18@gmail.com>
2023-09-30 18:18:39 +0800

    Style filter settings

--- src/view/components/ProductListSettings.tsx
+++ src/view/components/ProductListSettings.tsx
@@ -10,11 +10,13 @@ import {
 } from "../../utilities.ts";
 
 function FieldSet(props: { legend: string } & ComponentProps<"fieldset">) {
-  const { legend, children } = props;
+  const { legend, className, children } = props;
   return (
-    <fieldset {...props}>
-      <legend>{legend}</legend>
-      {children}
+    <fieldset {...props} className="grid gap-2">
+      <legend className="float-left uppercase font-bold text-sm tracking-widest">
+        {legend}
+      </legend>
+      <section className={className}>{children}</section>
     </fieldset>
   );
 }
@@ -39,7 +41,7 @@ function PriceInputs() {
 
   const inputBoxMin = (
     <input
-      className="w-full text-black"
+      className="w-full text-black px-2 py-1 text-sm tracking-wide"
       type="number"
       name="priceMin"
       value={Number.isNaN(priceMin) ? lowest : priceMin}
@@ -48,7 +50,7 @@ function PriceInputs() {
   );
   const inputBoxMax = (
     <input
-      className="w-full text-black"
+      className="w-full text-black px-2 py-1 text-sm tracking-wide"
       type="number"
       name="priceMax"
       value={Number.isNaN(priceMax) ? highest : priceMax}
@@ -79,8 +81,11 @@ function PriceInputs() {
   );
 
   return (
-    <FieldSet legend={"Price"} className="grid gap-3">
-      <section className="flex gap-3 mx-auto">
+    <FieldSet
+      legend={"Price"}
+      className="grid gap-2 text-xs uppercase tracking-widest"
+    >
+      <section className="flex items-center gap-3 mx-auto">
         <label className="w-1/3">
           <span className="hidden">Min</span>
           {inputBoxMin}
@@ -91,11 +96,11 @@ function PriceInputs() {
           {inputBoxMax}
         </label>
       </section>
-      <label className="grid grid-cols-[1fr_5fr]">
+      <label className="grid grid-cols-[1fr_5fr] items-center ">
         Min
         {inputSliderMin}
       </label>
-      <label className="grid grid-cols-[1fr_5fr]">
+      <label className="grid grid-cols-[1fr_5fr] items-center ">
         Max
         {inputSliderMax}
       </label>
@@ -128,10 +133,14 @@ function BrandFilters({ formRef }: { formRef: RefObject<HTMLFormElement> }) {
   brands = [...new Set(vehicles.map(({ brand }) => brand))];
   brands = brands.sort((a, b) => (a < b ? -1 : 1));
 
+  const cls = C(
+    "flex gap-2 items-center mb-2",
+    "truncate text-xs uppercase font-thin tracking-widest"
+  );
   return (
     <FieldSet legend={"Brand"} className="columns-2" onInput={filter}>
       {brands.map((brand) => (
-        <label key={brand} className="flex gap-2 items-center truncate">
+        <label key={brand} className={cls}>
           <input type="checkbox" name="brand" value={brand} />
           {brand}
         </label>
@@ -145,12 +154,14 @@ export function ProductListSettings() {
 
   const cls = C(
     "h-full overflow-y-auto",
+    "grid auto-rows-min gap-5",
     "scrollbar-thin scrollbar-track-neutral-500 scrollbar-thumb-neutral-600"
   );
   return (
     <aside className="h-full overflow-hidden">
       <form ref={formRef} className={cls}>
         <BrandFilters formRef={formRef} />
+        <hr className="border-neutral-600" />
         <PriceInputs />
       </form>
     </aside>
